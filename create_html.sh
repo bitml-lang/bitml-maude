e4d7c19efb0a2048dcefc8779a567472539cf48f
Nicola Atzei <atzeinicola@gmail.com>
2018-07-23 15:05:38 +0200

    Update create_html.sh

--- create_html.sh
+++ create_html.sh
@@ -1,5 +1,10 @@
 DATE=`date +"%a, %e %b %Y %H:%M %:z"`
-TEXT="***\n*** Last update: $DATE\n***\n"
+TEXT="***
+*** Last update: $DATE
+***
+*** Github: https://github.com/natzei/bitml-sem/blob/master/bitml.maude
+***
+"
 
 echo "Creating index.html"
 echo -e "$TEXT" | cat - $1/bitml.maude | pygmentize -l $1/pygments_maude.py:MaudeLexer -x -f html -O full=true -o  $1/index.html
