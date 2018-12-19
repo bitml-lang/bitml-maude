647345d08f5747d349f7b24786dbed90c6f4de69
Nicola Atzei <atzeinicola@gmail.com>
2018-12-19 11:42:25 +0100

    create_html.sh

--- create_html.sh
+++ create_html.sh
@@ -1,10 +1,21 @@
+rm -r html
+mkdir -p html
+
 DATE=`date +"%a, %e %b %Y %H:%M %:z"`
-TEXT="***
+
+for X in `ls -1 *.maude`; do
+    if [ -f $X ]; then
+        TEXT="***
 *** Last update: $DATE
 ***
-*** Github: https://github.com/natzei/bitml-maude/blob/master/bitml.maude
+*** Github: https://github.com/natzei/bitml-maude/blob/master/$X
 ***
 "
+        extension="${X##*.}"
+        filename="${X%.*}"
+        echo "Creating $filename.html"
+        echo -e "$TEXT" | cat - $1/$X | pygmentize -l $1/pygments_maude.py:MaudeLexer -x -f html -O full=true -o  $1/html/$filename.html
+    fi
+done
 
-echo "Creating index.html"
-echo -e "$TEXT" | cat - $1/bitml.maude | pygmentize -l $1/pygments_maude.py:MaudeLexer -x -f html -O full=true -o  $1/index.html
+cp html/bitml.html html/index.html
